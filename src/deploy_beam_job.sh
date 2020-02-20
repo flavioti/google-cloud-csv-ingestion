7ff4dba0a9196be4aa13ba9a41967c2eb5df6b42
flavio <flaviomarcioti@gmail.com>
2020-02-20 05:51:40 -0300

    Liberação Alpha

--- src/deploy_beam_job.sh
+++ src/deploy_beam_job.sh
@@ -1,6 +1,6 @@
 export PROJECT=treinamento-254613
 
-python ./src/dataflow/main2.py \
+python ./src/dataflow/main.py \
   --runner DataflowRunner \
   --project $PROJECT \
   --temp_location gs://$PROJECT/tmp/ \
